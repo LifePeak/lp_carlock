816995368059ee9da8afbc55a3789120d6088503
Lukas Siegfried Heinz <lukas.s.heinz@stud.h-da.de>
2022-09-18 15:00:11 +0200

    hotfix readme

--- config.lua
+++ config.lua
@@ -20,9 +20,8 @@ Config.Notification.System = "none", -- lp_notify / none
 Config.Notification.postion = "top right" -- Only works lp_notify! | lp_"top right", [top Left, top Right, bottom Left, bottom Right]
 Config.Notification.displaytime = 13000 -- ms
 
-
 Config.AdminGroups = {
 	superadmin = true,
 	admin = true
 }
--- also give player icepermisson to admincarlockkeys
+-- You can also give player AcePerms to admincarlockkeys instead
