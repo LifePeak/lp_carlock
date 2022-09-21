d38ef9a7a41521ebe498ef286812c2a6602ed5f6
Lukas Siegfried Heinz <lukas.s.heinz@stud.h-da.de>
2022-09-21 19:10:05 +0200

    uploading preview images

--- config.lua
+++ config.lua
@@ -15,7 +15,7 @@ Config.PlayerCarArea = 30
 Config.Locale  = 'de'
 Config.EnableJobvehicle = true -- players can open all vehicle with same job.
 Config.Notification = {}
-Config.Notification.System = "none" -- lp_notify / none
+Config.Notification.System = "lp_notify" -- lp_notify / none
 Config.Notification.Postion = "top right" -- Only works lp_notify! | lp_"top right", [top Left, top Right, bottom Left, bottom Right]
 Config.Notification.Displaytime = 1000 -- ms
 
